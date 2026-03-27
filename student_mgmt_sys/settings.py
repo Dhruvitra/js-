932a0f96e2cc1d70130a948ee2801d5e028545cb
Dhruvitra <dhruviktra.rajput.1379@gmail.com>
2026-03-27 20:48:57 +0530

    Hardcode ALLOWED_HOSTS to '*'

--- student_mgmt_sys/settings.py
+++ student_mgmt_sys/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cu@8syvl!rm_fy-#v%r)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
