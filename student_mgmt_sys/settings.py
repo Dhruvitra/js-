9cf21e5183e2b2a118b370ff630eb7e8b7007554
Dhruvitra <dhruviktra.rajput.1379@gmail.com>
2026-03-27 20:45:29 +0530

    Allow all hosts so Render deployment works

--- student_mgmt_sys/settings.py
+++ student_mgmt_sys/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-cu@8syvl!rm_fy-#v%r)x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
